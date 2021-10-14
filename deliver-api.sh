9196f9b3d97fc084513bb5254c5e41ea538c20aa
Antonio Forgianni <antonio.forgianni@mail.com>
2021-10-14 11:15:50 +0200

    local development support

---
+++ deliver-api.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "mv ./local/comm-api/*.js ./app/packages/comm-api/"
+
+mv ./local/comm-api/*.js ./app/packages/comm-api/
---
+++ develop-api.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "mv ./app/packages/comm-api/*.js ./local/comm-api/"
+
+mv ./app/packages/comm-api/*.js ./local/comm-api/
