14838be82209cf01d423553aef174e05d61a1cd2
Antonio Forgianni <antonio.forgianni@mail.com>
2021-10-17 16:17:42 +0200

    graphic improvements

--- app/web/src/routes/Bedrock.svelte
+++ app/web/src/routes/Bedrock.svelte
@@ -13,9 +13,9 @@
 
 <div class="mb-3">
 	<p>
-		Questa app si fonda sulla <span class="fst-italic"
+		CommunityApp si fonda sulla <span class="fst-italic"
 			>Parola di Dio in Salmo 133</span
-		>.<br /> Da qui nasce il suo motto
+		>. Da qui nasce il suo motto
 		<span class="fst-italic"
 			>Psalm 133 &#8231; It&rsquo;s Good Together!</span
 		>.
