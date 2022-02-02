d62e68f2fd03ec2994f22466b25f6ce2ddf5dc33
Antonio Forgianni <antonio.forgianni@mail.com>
2022-02-02 15:21:48 +0100

    cleaning (2.0)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CommunityApp
 
 <p>A web application to share and propose lyrics or readings between people during a meeting.</p>
+
+v2.0-swan
--- v1.0/web/src/routes/Index.svelte
+++
@@ -1,7 +0,0 @@
-<!-- Qui ci sarà un elenco di tutte le community -->
-
-<div class="mb-3">
-	<p>
-		<img src="images/snorlax.png" class="img-fluid" alt="snorlax" />
-	</p>
-</div>
