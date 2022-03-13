a1ba261772e5f217f2626143490299e8edbca507
Antonio Forgianni <antonio.forgianni@mail.com>
2022-03-13 16:45:50 +0100

    refactoring

--- server/cmm-api/cmm-business/src/main/java/app/community/business/common/exception/ErrorMessage.java
+++ server/cmm-api/cmm-business/src/main/java/app/community/business/common/exception/ErrorMessage.java
@@ -2,9 +2,14 @@ package app.community.business.common.exception;
 
 public class ErrorMessage {
 
+	//COMMUNITY
 	public static final String COMMUNITY_NOT_FOUND_MSG = "Community not found";
 	public static final String COMMUNITY_AUTHENTICATION_NOT_FOUND_MSG = "Community authentication not found";
-	public static final String UNAUTHORIZED_MSG = "Unauthorized";
+
+	//CONTENT
 	public static final String CONTENT_ALREADY_PROPOSED_MSG = "Content already proposed";
+	
+	//GENERAL
+	public static final String UNAUTHORIZED_MSG = "Unauthorized";
 	
 }
